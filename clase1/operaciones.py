ec0a52aef1ef491ccbd534d071ad4c9b5e6d21b4
Urielglb <uriel_06@ciencias.unam.mx>
2019-09-27 23:50:28 -0500

    Primera clase

---
+++ clase1/operaciones.py
@@ -0,0 +1,115 @@
+"""
+Suma
+"""
+print(2+3)
+"""
+Resta
+"""
+print(2-3)
+"""
+Multiplicación
+"""
+print(3*2)
+"""
+División
+"""
+print(3/2)
+"""
+Division entera
+"""
+print(3//2)
+"""
+ Exponente
+"""
+print(3**2)
+"""
+Módulo
+"""
+print(3%2)
+
+"""
+Concatenación
+"""
+nombre = "Uriel"
+apellido = "García Luna"
+print(nombre + " " + apellido)
+"""
+Multiplicación
+"""
+mensaje = "Hola"
+print(mensaje*3)
+"""
+Upper
+"""
+minusculas = "hola"
+print(minusculas.upper())
+"""
+Lower
+"""
+mayusculas = "HOLA"
+print(mayusculas.lower())
+"""
+Title
+"""
+pelicula = "inglorious bastards"
+print(pelicula.title())
+"""
+Capitalize
+"""
+print(pelicula.capitalize())
+"""
+Strip
+"""
+mensaje = "       Hola"
+print(mensaje.strip())
+"""
+Strip izquierda
+"""
+print(mensaje.lstrip())
+"""
+Strip derecha
+"""
+print(mensaje.rstrip())
+"""
+Mayor que
+"""
+print(9>9)
+"""
+Mayor o igual que
+"""
+print(9>=9)
+"""
+Menor que 
+"""
+print(9<10)
+"""
+Menor o igual que
+"""
+print(10<=10)
+"""
+Igual
+"""
+print(14==15)
+"""
+Diferente
+"""
+print(15!=16)
+"""
+Operador and 
+"""
+print(True and False)
+"""
+Operador or
+"""
+print(True or False)
+"""
+Recibir una entrada del usuario
+"""
+nombre = input("¿Como te llamas?")
+print("Hola "+nombre)
+"""
+Transformar el tipo de dato que se recibe en la entrada(es string)
+"""
+edad = input("¿Cuantos años tienes?")
+edad_aumentada = int(edad)+2
+print("Tu edad + 2 es "+ str(edad_aumentada))
